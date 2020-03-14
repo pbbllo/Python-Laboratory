1c23ae5f71d2803f0358b69aa15088c6d8e3be74
IamShiro <42015013+IamShiro@users.noreply.github.com>
2020-03-14 12:09:18 -0300

    Delete 1618.py

--- 1618.py
+++
@@ -1,9 +0,0 @@
-#URI Questions
-#Python
-#Colisão
-for n in range(int(input())):
-    Ax,Ay,Bx,By,Cx,Cy,Dx,Dy,Rx,Ry = [int(x) for x in input().split()]
-    if Ax <= Rx <= Bx and Ay <= Ry <= Dy:
-        print('1')
-    else:
-        print('0')
