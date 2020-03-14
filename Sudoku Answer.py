a525e5419a96d04423973fffb43eeb6c2608e163
IamShiro <42015013+IamShiro@users.noreply.github.com>
2020-03-14 11:32:25 -0300

    Sudoku Answer.py
    Program to confirm the answer

--- Sudoku Answer.py
+++ Sudoku Answer.py
@@ -1,6 +1,6 @@
 # Uri-Questions
 #Python
-#Sudoku
+#Program to confirm the answer
 for n in range(int(input())): #números de casos testes
     sudoku = [[int(x) for x in input().split()]for y in range(9)]
     solve = True
