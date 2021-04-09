b05cf279cde172b105925ea0f8464f26c61a272c
IamShiro <pabloweslleywork@gmail.com>
2021-04-09 01:02:38 -0300

    Created fly_food.py

--- fly_food.py
+++ fly_food.py
@@ -1,15 +1,13 @@
-def minDistance(matriz):
-    return string
-
-
-
-
-
-
-
 lines, columns = map(int, input().split())
-matriz = [[input().split()] for i in range(lines)]   #O(n)
+matriz = [input().split() for i in range(lines)]    #O(n)
+coordinates = []
 
+for line in range(lines):
+    for column in range(columns):
+        if matriz[line][column] != "0":
+            coordinate = (matriz[line][column], line, column)
+            coordinates.append(coordinate)
 
-print(matriz)
+print(coordinates)
 
+#print(minDistance(matriz, lines, columns))
