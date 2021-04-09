562ab7a46ca96d591f64d86a27bb258d1bc1590c
IamShiro <pabloweslleywork@gmail.com>
2021-04-09 01:03:48 -0300

    Updated fly_food.py

--- fly_food.py
+++ fly_food.py
@@ -1,12 +1,26 @@
 lines, columns = map(int, input().split())
-matriz = [input().split() for i in range(lines)]    #O(n)
+matriz = [input().split() for i in range(lines)]
 coordinates = []
+start = 0
 
-for line in range(lines):
+for line in range(lines):                               # O(n ** 2)
     for column in range(columns):
         if matriz[line][column] != "0":
             coordinate = (matriz[line][column], line, column)
             coordinates.append(coordinate)
+        if matriz[line][column] == "R":
+            start = len(coordinates) - 1
+
+def minDistance(coordinates, p, lines, columns)
+    distance = lines * columns
+    for point in coordinates:
+        a = p[1] - point[1]
+        b = p[2] - point[2]
+        soma = abs(a + b)
+        
+        if soma <= distance:
+            return 0
+
 
 print(coordinates)
 
