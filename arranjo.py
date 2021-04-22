9e3c8e15ca47a57a39c0468e820ccef0c0d143c1
IamShiro <pabloweslleywork@gmail.com>
2021-04-21 22:20:30 -0300

    Updated arranjo.py

--- arranjo.py
+++ arranjo.py
@@ -1,8 +1,37 @@
-def arranjo(elements, string):
+import math
+
+def arranjo(elements, path, array):
     if len(elements) == 0:
-        print(string)
+        array.append(path)
+        return array
     else:
         for i, element in enumerate(elements):
-            arranjo(elements[:i] + elements[i + 1:], string + element)
+            array = arranjo(elements[:i] + elements[i + 1:], path + element, array)
+            
+    return array
+
+def betterDistance(array, cities, start):
+    betterPath = math.inf
+
+    return betterPath
+
+    
+'''
+cities = []
+elements = []
+start = None    
+lines, columns = map(int, input().split())
+for l in range(lines):
+    line = input().split()
+    for c in range(columns):
+        if line[c] == "R":
+            start = (line[c], l, c)
+        elif line[c] != "R" and line[c] != "0":
+            cities.append((line[c], l, c))
+            elements.append(line[c])
+'''
+
 elements = input().split()
-arranjo(elements, "")
+array = []
+array = arranjo(elements, "", array)
+print(array)
